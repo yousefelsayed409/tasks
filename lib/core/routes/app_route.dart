c6b2f84c1a37aa2148c57be3732b52bc1d6a0651
yousefelsayed <eyousef464@gmail.com>
2025-01-23 22:28:14 +0200

    feat: add route for sign-up screen in app routing

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tasks/feature/auth/signin/presentation/view/signin_view.dart';
+import 'package:tasks/feature/auth/signup/presentation/view/sign_up_view.dart';
 
 
 abstract class AppRoute {
@@ -12,7 +13,8 @@ abstract class AppRoute {
     switch (settings.name) {
       case signInScreen:
         return MaterialPageRoute(builder: (_) => const SignInScreen());
-      
+      case signUpScreen:
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(

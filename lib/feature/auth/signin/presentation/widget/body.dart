e0d35209a10e4dd3170083e59e933b4bc50e8165
yousefelsayed <eyousef464@gmail.com>
2025-01-23 22:19:13 +0200

    feat: add sign-in body widget with welcome message and layout

---
+++ lib/feature/auth/signin/presentation/widget/body.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tasks/core/utils/app_text_style.dart';
+
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: (20.h)),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 7),
+                  Text("Welcome  ", style: AppStyles.textStyle28),
+                  SizedBox(height: 8.h),
+                  const Text(
+                    "Please enter your details to proceed",
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 15.h),
+                
+                  SizedBox(height: 10.h),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Forgot Password ?",
+                      style: AppStyles.textStyle16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50.h,
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "or sign up with",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
